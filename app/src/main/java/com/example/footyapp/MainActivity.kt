2651f0ec4abeca584cad59d2b0cb271d7ed8512a
DonDangles <awallace.anthony@gmail.com>
2020-08-08 20:56:23 -0400

    log commit

--- app/src/main/java/com/example/footyapp/MainActivity.kt
+++ app/src/main/java/com/example/footyapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.footyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //log
+        Log.d("MainActivity", "L565656565")
     }
 }
