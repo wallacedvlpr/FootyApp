24a4f50c7bb1a5dbe4960f5e014effd74ff66412
andrew <andrew22.wallace@gmail.com>
2020-08-20 13:17:45 -0400

    refactored Now using only two layouts/recycler views refactored the view id's
    Signed-off-by: andrew <andrew22.wallace@gmail.com>

--- app/src/main/java/com/example/footyapp/view/fragments/ClubsFragment.kt
+++ app/src/main/java/com/example/footyapp/view/fragments/ClubsFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.footyapp.R
 import com.example.footyapp.model.ClubItem
-import kotlinx.android.synthetic.main.clubs_frag_list_layout.*
+import kotlinx.android.synthetic.main.main_fragment_list_layout.*
 
 class ClubsFragment private constructor(): Fragment(){
     private val adapter by lazy {
@@ -23,20 +23,22 @@ class ClubsFragment private constructor(): Fragment(){
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         adapter.activity = activity
-        return inflater.inflate(R.layout.clubs_frag_list_layout,container, false)
+        return inflater.inflate(R.layout.main_fragment_list_layout,container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initRecyclerView()
+        initViews()
     }
     fun getDataSet(data: List<ClubItem>){
         adapter.dataSet = data
     }
 
-    private fun initRecyclerView(){
-        club_frag_recycler_view.layoutManager = LinearLayoutManager(activity)
-        club_frag_recycler_view.adapter = adapter
+    private fun initViews(){
+        val title = "Clubs"
+        main_frag_list_title.text = title
+        main_frag_recycler_view.layoutManager = LinearLayoutManager(activity)
+        main_frag_recycler_view.adapter = adapter
     }
 
     companion object{
--- app/src/main/java/com/example/footyapp/view/fragments/LeaguesFragment.kt
+++ app/src/main/java/com/example/footyapp/view/fragments/LeaguesFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.footyapp.R
 import com.example.footyapp.model.League
-import kotlinx.android.synthetic.main.leagues_frag_list_layout.*
+import kotlinx.android.synthetic.main.main_fragment_list_layout.*
 
 class LeaguesFragment private constructor(): Fragment(){
 
@@ -24,21 +24,23 @@ class LeaguesFragment private constructor(): Fragment(){
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         adapter.activity = activity
-        return inflater.inflate(R.layout.leagues_frag_list_layout, container, false)
+        return inflater.inflate(R.layout.main_fragment_list_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initRecyclerView()
+        initViews()
     }
 
     fun getDataSet(data: List<League>){
         adapter.dataSet = data
     }
 
-    private fun initRecyclerView(){
-        leagues_frag_recycler_view.layoutManager = LinearLayoutManager(activity)
-        leagues_frag_recycler_view.adapter = adapter
+    private fun initViews(){
+        val title = "Leagues"
+        main_frag_list_title.text = title
+        main_frag_recycler_view.layoutManager = LinearLayoutManager(activity)
+        main_frag_recycler_view.adapter = adapter
     }
 
     companion object{
